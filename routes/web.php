aa5b4a22e03e064c88f89931e6b76a3a75a806f9
Pietro Giordano <giopi87@gmail.com>
2023-05-23 20:19:33 +0200

    rotte

--- routes/web.php
+++ routes/web.php
@@ -1,7 +1,11 @@
 <?php
 
-use App\Http\Controllers\ProfileController;
 use Illuminate\Support\Facades\Route;
+// Controllers
+use App\Http\Controllers\ProfileController;
+use App\Http\Controllers\ProjectController;
+use App\Http\Controllers\SkillController;
+use App\Http\Controllers\MessageController;
 
 /*
 |--------------------------------------------------------------------------
@@ -15,17 +19,23 @@ use Illuminate\Support\Facades\Route;
 */
 
 Route::get('/', function () {
-    return view('welcome');
+      return view('welcome');
 });
 
 Route::get('/dashboard', function () {
-    return view('dashboard');
+      return view('dashboard');
 })->middleware(['auth', 'verified'])->name('dashboard');
 
-Route::middleware('auth')->group(function () {
-    Route::get('/profile', [ProfileController::class, 'edit'])->name('profile.edit');
-    Route::patch('/profile', [ProfileController::class, 'update'])->name('profile.update');
-    Route::delete('/profile', [ProfileController::class, 'destroy'])->name('profile.destroy');
+Route::middleware(['auth', 'verified'])->group(function () {
+      Route::resource('projects', ProjectController::class);
+      Route::resource('skills', SkillController::class);
+      Route::resource('messages', MessageController::class);
 });
 
-require __DIR__.'/auth.php';
+Route::middleware('auth')->group(function () {
+      Route::get('/profile', [ProfileController::class, 'edit'])->name('profile.edit');
+      Route::patch('/profile', [ProfileController::class, 'update'])->name('profile.update');
+      Route::delete('/profile', [ProfileController::class, 'destroy'])->name('profile.destroy');
+});
+
+require __DIR__ . '/auth.php';
