9d8456b5e1f30a9464a10ddb6bdaa0807626226a
Pietro Giordano <giopi87@gmail.com>
2023-05-28 19:59:56 +0200

    aggiunta script dropdown index

--- resources/views/admin/projects/index.blade.php
+++ resources/views/admin/projects/index.blade.php
@@ -25,16 +25,26 @@
                                           <a href="" class="border border-gray-300 py-1 px-2 rounded-lg font-bold hover:bg-gray-100">
                                                 View project
                                           </a>
-                                          <button>
+
+                                          <button id="showDropdown">
                                                 <svg xmlns="http://www.w3.org/2000/svg" fill="none" viewBox="0 0 24 24" stroke-width="1.5" stroke="currentColor" class="w-6 h-6 text-gray-500 hover:text-gray-900">
                                                       <path stroke-linecap="round" stroke-linejoin="round" d="M12 6.75a.75.75 0 110-1.5.75.75 0 010 1.5zM12 12.75a.75.75 0 110-1.5.75.75 0 010 1.5zM12 18.75a.75.75 0 110-1.5.75.75 0 010 1.5z" />
                                                 </svg>
                                           </button>
                                           
-                                          <div class="absolute right-0 top-full z-10 w-48 rounded-md bg-white py-1 shadow-lg ring-1 ring-black ring-opacity-5 focus:outline-none">
+                                          {{-- dropdown menu --}}
+                                          <div id="dropdown" style="display:none" class="absolute right-0 top-full z-10 w-48 rounded-md bg-white py-1 shadow-lg ring-1 ring-black ring-opacity-5 focus:outline-none">
                                                 <a href="#" class="block px-4 py-2 text-sm text-gray-700 hover:bg-gray-100">Update</a>
                                                 <a href="#" class="block px-4 py-2 text-sm text-gray-700 hover:bg-gray-100">Delete</a>
                                           </div>
+
+                                          {{-- script show/hidden dropdown menu --}}
+                                          <script>
+                                                document.getElementById('showDropdown').addEventListener('click', function() {
+                                                      const dropdown = document.getElementById('dropdown');
+                                                      dropdown.style.display = (dropdown.style.display === 'none') ? 'block' : 'none';
+                                                });
+                                          </script>
                                     </div>
                               </li>
                        </ul>
