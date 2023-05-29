355f4b4a4051d58e74209dbc4c0a9dc2220e2d18
Pietro Giordano <giopi87@gmail.com>
2023-05-29 15:14:29 +0200

    aggiunta animazione dropdown con alpinejs

--- resources/views/admin/projects/create.blade.php
+++ resources/views/admin/projects/create.blade.php
@@ -4,17 +4,24 @@
                   {{ __('Projects') }}
             </h2>
 
-            <a href="{{ route('admin.projects.index') }}" class="border-2 border-gray-800 py-1 px-3 rounded-full text-sm hover:bg-gray-800 hover:text-white">
+            <a href="{{ route('admin.projects.index') }}" class="border-2 border-gray-500 py-1 px-3 rounded-full text-sm hover:bg-gray-800 hover:text-white">
                   Back
             </a>
       </x-slot>
 
-      <div class="py-8">
+      <div class="py-10">
             <div class="max-w-7xl mx-auto sm:px-6 lg:px-8">
                   <div class="bg-white overflow-hidden shadow-sm sm:rounded-lg">
-                        <div class="p-6 text-gray-900">
-                              {{ __("You're logged in!") }}
-                        </div>
+
+                        <form action="{{ route('admin.projects.store') }}" method="POST" enctype="multipart/form-data" class="pt-8 pb-20 md:px-2 lg:px-4">
+                              @csrf
+
+                              <div>
+                                    <label for="name">Nome progetto</label>
+                                    <input type="text" id="name" name="name" placeholder="Inserire nome progetto...">
+                              </div>
+                        </form>
+
                   </div>
             </div>
       </div>
--- resources/views/admin/projects/index.blade.php
+++ resources/views/admin/projects/index.blade.php
@@ -13,7 +13,7 @@
             <div class="max-w-7xl mx-auto sm:px-6 lg:px-8">
                   <div class="bg-white overflow-hidden shadow-sm sm:rounded-lg">
 
-                       <ul role="list" class="divide-y divide-gray-100 pt-8 pb-20 md:px-2 lg:px-4">
+                        <ul role="list" class="divide-y divide-gray-100 pt-8 pb-20 md:px-2 lg:px-4">
                               <li class="flex justify-between items-center gap-x-6 p-5">
                                     <div class="flex gap-x-4">
                                           <div class="min-w-0 flex-auto">
@@ -21,30 +21,25 @@
                                                 <p class="mt-1 truncate text-xs leading-5 text-gray-500">Replica fedele whatsapp web</p>
                                           </div>
                                     </div>
-                                    <div class="hidden sm:flex sm:justify-end sm:items-center gap-x-4 relative">
+
+                                    {{-- tramite il framework Alpine.js si inizializza una variabile open e tramite eventi si tiene traccia dello stato chiuso/aperto, si evita che si propaghi al genitore e si fa si che cliccando al di fuori del dropdown open=false --}}
+                                    <div x-data="{ open: false }" @click.outside="open = false" @close.stop="open = false" class="hidden sm:flex sm:justify-end sm:items-center gap-x-4 relative">
                                           <a href="" class="border border-gray-300 py-1 px-2 rounded-lg font-bold hover:bg-gray-100">
                                                 View project
                                           </a>
 
-                                          <button id="showDropdown">
+                                          {{-- trigger del dropdown --}}
+                                          <button id="showDropdown" @click="open = ! open">
                                                 <svg xmlns="http://www.w3.org/2000/svg" fill="none" viewBox="0 0 24 24" stroke-width="1.5" stroke="currentColor" class="w-6 h-6 text-gray-500 hover:text-gray-900">
                                                       <path stroke-linecap="round" stroke-linejoin="round" d="M12 6.75a.75.75 0 110-1.5.75.75 0 010 1.5zM12 12.75a.75.75 0 110-1.5.75.75 0 010 1.5zM12 18.75a.75.75 0 110-1.5.75.75 0 010 1.5z" />
                                                 </svg>
                                           </button>
                                           
-                                          {{-- dropdown menu --}}
-                                          <div id="dropdown" style="display:none" class="absolute right-0 top-full z-10 w-32 rounded-md bg-white py-1 shadow-lg ring-1 ring-black ring-opacity-5 focus:outline-none">
+                                          {{-- dropdown menu, tramite Alpine.js (x-transition) si gestisce l'animazione di compsarsa/scomparsa --}}
+                                          <div x-show="open" x-transition:enter="transition ease-out duration-200" x-transition:enter-start="transform opacity-0 scale-95" x-transition:enter-end="transform opacity-100 scale-100" x-transition:leave="transition ease-in duration-75" x-transition:leave-start="transform opacity-100 scale-100" x-transition:leave-end="transform opacity-0 scale-95" @click="open = false" style="display:none" class="absolute right-0 top-full z-10 w-32 rounded-md bg-white py-1 shadow-lg ring-1 ring-black ring-opacity-5 focus:outline-none">
                                                 <a href="#" class="block px-4 py-2 text-sm text-gray-700 hover:bg-gray-100">Update</a>
                                                 <a href="#" class="block px-4 py-2 text-sm text-gray-700 hover:bg-gray-100">Delete</a>
                                           </div>
-
-                                          {{-- script show/hidden dropdown menu --}}
-                                          <script>
-                                                document.getElementById('showDropdown').addEventListener('click', function() {
-                                                      const dropdown = document.getElementById('dropdown');
-                                                      dropdown.style.display = (dropdown.style.display === 'none') ? 'block' : 'none';
-                                                });
-                                          </script>
                                     </div>
                               </li>
                        </ul>
