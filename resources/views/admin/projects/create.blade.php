09aeb7e734995030fe97a90f77dae94259afac0e
Pietro Giordano <giopi87@gmail.com>
2023-05-29 18:03:45 +0200

    avanzamento

--- resources/views/admin/projects/create.blade.php
+++ resources/views/admin/projects/create.blade.php
@@ -16,9 +16,19 @@
                         <form action="{{ route('admin.projects.store') }}" method="POST" enctype="multipart/form-data" class="pt-8 pb-20 md:px-2 lg:px-4">
                               @csrf
 
-                              <div>
-                                    <label for="name">Nome progetto</label>
-                                    <input type="text" id="name" name="name" placeholder="Inserire nome progetto...">
+                              <div class="p-3 lg:p-5">
+                                    <label for="name" class="block font-medium leading-6 text-gray-900">Nome progetto</label>
+                                    <input type="text" id="name" name="name" class="w-full md:w-1/2 text-sm mt-2 rounded-md shadow-sm border-0 ring-1 ring-inset ring-gray-300 focus-within:ring-2 focus-within:ring-inset focus-within:ring-indigo-600 sm:max-w-md" placeholder="Inserire nome progetto..." required min="6" max="255">
+                              </div>
+
+                              <div class="p-3 lg:p-5">
+                                    <label for="description" class="block font-medium leading-6 text-gray-900">Descrizione progetto</label>
+                                    <textarea id="description" name="description" rows="10" class="w-full md:w-2/3 text-sm mt-2 rounded-md shadow-sm border-0 ring-1 ring-inset ring-gray-300 focus-within:ring-2 focus-within:ring-inset focus-within:ring-indigo-600 sm:max-w-md" placeholder="Inserire descrizione progetto..." required min="6" max="255"></textarea>
+                              </div>
+
+                              <div class="p-3 lg:p-5">
+                                    <label for="video" class="block font-medium leading-6 text-gray-900">Descrizione progetto</label>
+                                    <input type="file" id="video" name="video" class="text-sm mt-2 rounded-md shadow-sm border-0 ring-1 ring-inset ring-gray-300 focus-within:ring-2 focus-within:ring-inset focus-within:ring-indigo-600 sm:max-w-md" placeholder="Inserire descrizione progetto..." required min="6" max="255">
                               </div>
                         </form>
 
