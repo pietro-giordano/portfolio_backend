bb117342c592176a73b2978131cfbce780d7001b
Pietro Giordano <giopi87@gmail.com>
2023-05-30 18:00:54 +0200

    fix

--- resources/views/admin/skills/create.blade.php
+++ resources/views/admin/skills/create.blade.php
@@ -4,7 +4,7 @@
                   {{ __('Skills') }}
             </h2>
 
-            <a href="{{ route('admin.projects.index') }}" class="border-2 border-gray-500 py-1 px-3 rounded-full text-sm hover:bg-gray-800 hover:text-white">
+            <a href="{{ route('admin.skills.index') }}" class="border-2 border-gray-500 py-1 px-3 rounded-full text-sm hover:bg-gray-800 hover:text-white">
                   Back
             </a>
       </x-slot>
@@ -13,7 +13,7 @@
             <div class="max-w-7xl mx-auto sm:px-6 lg:px-8">
                   <div class="bg-white overflow-hidden shadow-sm sm:rounded-lg">
 
-                        <form action="{{ route('admin.projects.store') }}" method="POST" enctype="multipart/form-data" class="pt-8 pb-20 md:px-2 lg:px-4">
+                        <form action="{{ route('admin.skills.store') }}" method="POST" enctype="multipart/form-data" class="pt-8 pb-20 md:px-2 lg:px-4">
                               @csrf
 
                               <div class="px-3 pb-3 lg:px-5 lg:pb-5 text-xs">
@@ -26,7 +26,7 @@
                               </div>
 
                               <div class="px-3 pb-3 lg:px-5 lg:pb-5">
-                                    <label for="description" class="block font-medium leading-6 text-gray-900">Descrizione progetto</label>
+                                    <label for="description" class="block font-medium leading-6 text-gray-900">Descrizione skill</label>
                                     <textarea id="description" name="description" rows="10" class="w-full md:w-2/3 text-sm mt-2 rounded-md shadow-sm border-0 ring-1 ring-inset ring-gray-300 focus-within:ring-2 focus-within:ring-inset focus-within:ring-indigo-600 sm:max-w-md" placeholder="Inserire descrizione skill..." min="20" max="65000"></textarea>
                               </div>
 
